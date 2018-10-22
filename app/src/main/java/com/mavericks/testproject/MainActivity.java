ae8e320a6bedcaacc7b1d7d6ac143d3b09276515
peter <pb@calldorado.com>
2018-10-22 14:10:21 +0200

    Added settings and search buttons + cdo 5.4.1.1710

--- app/src/main/java/com/mavericks/testproject/MainActivity.java
+++ app/src/main/java/com/mavericks/testproject/MainActivity.java
@@ -2,8 +2,11 @@ package com.mavericks.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.calldorado.Calldorado;
+import com.calldorado.manual_search.CDOPhoneNumber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calldorado.startCalldorado(this, savedInstanceState);
+        Calldorado.startCalldorado(this, null, savedInstanceState, new Calldorado.CalldoradoFullCallback() {
+            @Override
+            public void onInitDone(boolean b, String[] strings, int[] ints) {
+                Toast.makeText(MainActivity.this, "Calldorado ready!", Toast.LENGTH_SHORT).show();
+            }
+        });
         Calldorado.setCalldoradoAftercallColors(this,
                 getResources().getColor(R.color.white),
                 getResources().getColor(R.color.light_grey),
                 getResources().getColor(R.color.black)
         );
+    }
+
+    public void onSettings(View view) {
+        Calldorado.createCalldoradoSettingsActivity(this);
+    }
+
+    public void onSearch(View view) {
+        Calldorado.search(this, new CDOPhoneNumber("1111"));
     }
 }
