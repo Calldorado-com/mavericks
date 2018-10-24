231393e3911491fd2db40720f0fd0d35a562d07f
peter <pb@calldorado.com>
2018-10-24 21:27:55 +0200

    Settings update

--- app/src/main/java/com/mavericks/testproject/MainActivity.java
+++ app/src/main/java/com/mavericks/testproject/MainActivity.java
@@ -10,22 +10,13 @@ import com.calldorado.manual_search.CDOPhoneNumber;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Bundle savedInstanceState;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
         setContentView(R.layout.activity_main);
-
-        Calldorado.startCalldorado(this, null, savedInstanceState, new Calldorado.CalldoradoFullCallback() {
-            @Override
-            public void onInitDone(boolean b, String[] strings, int[] ints) {
-                Toast.makeText(MainActivity.this, "Calldorado ready!", Toast.LENGTH_SHORT).show();
-            }
-        });
-        Calldorado.setCalldoradoAftercallColors(this,
-                getResources().getColor(R.color.white),
-                getResources().getColor(R.color.light_grey),
-                getResources().getColor(R.color.black)
-        );
     }
 
     public void onSettings(View view) {
@@ -34,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSearch(View view) {
         Calldorado.search(this, new CDOPhoneNumber("1111"));
+    }
+
+    public void onStart(View view) {
+        Calldorado.startCalldorado(this, null, savedInstanceState, new Calldorado.CalldoradoFullCallback() {
+            @Override
+            public void onInitDone(boolean b, String[] strings, int[] ints) {
+                Toast.makeText(MainActivity.this, "Calldorado ready!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Calldorado.setCustomSettingsFeatures(this, true, true, false, false, true, true, false, false, true, true, false);
+        Calldorado.setCalldoradoAftercallColors(this,
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.light_grey),
+                getResources().getColor(R.color.black)
+        );
     }
 }
