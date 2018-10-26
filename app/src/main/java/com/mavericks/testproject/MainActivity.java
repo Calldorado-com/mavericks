d3eaf79336f204d7a2676dbab59764c7d99982b2
peter <pb@calldorado.com>
2018-10-26 22:22:42 +0200

    buildgradle out! for protected purposes

--- app/src/main/java/com/mavericks/testproject/MainActivity.java
+++ app/src/main/java/com/mavericks/testproject/MainActivity.java
@@ -1,7 +1,7 @@
 package com.mavericks.testproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -28,13 +28,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStart(View view) {
+        Calldorado.setCustomSettingsFeatures(MainActivity.this, true, true, false, false, true, true, false, false, true, true, false);
         Calldorado.startCalldorado(this, null, savedInstanceState, new Calldorado.CalldoradoFullCallback() {
             @Override
             public void onInitDone(boolean b, String[] strings, int[] ints) {
                 Toast.makeText(MainActivity.this, "Calldorado ready!", Toast.LENGTH_SHORT).show();
             }
         });
-        Calldorado.setCustomSettingsFeatures(this, true, true, false, false, true, true, false, false, true, true, false);
         Calldorado.setCalldoradoAftercallColors(this,
                 getResources().getColor(R.color.white),
                 getResources().getColor(R.color.light_grey),
